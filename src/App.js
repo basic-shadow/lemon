d8cf4824a93ee07c8c57e1074cd55616c411dcca
basic-shadow <islambek.mamet@nu.edu.kz>
2020-12-10 13:18:47 -0500

    a

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ class App extends React.Component {
     }
 
     async componentDidMount() {
-        const t1 = gsap.timeline({ defaults: { ease: "power1.out" } });
-        t1.fromTo(this.myRef2, { y: '20%' }, { y: '-100%', duration: 1.2 });
-        t1.to(this.myText1, { opacity: 1, x: '0%', transform: 'translateY(0%)', duration: 1});
-        t1.to(this.myText2, { opacity: 1, x: '0%', transform: 'translateY(0%)', duration: 2 });
-        t1.fromTo(this.myRef3, { opacity: 1 }, { opacity: 0, duration: 0.1 }, 4)
-            .fromTo(this.myRef1, { opacity: 0, x: '80%' }, { opacity: 1, x: '0%', duration: 1.5 }, 4);
+        // const t1 = gsap.timeline({ defaults: { ease: "power1.out" } });
+        // t1.fromTo(this.myRef2, { y: '20%' }, { y: '-100%', duration: 1.2 });
+        // t1.to(this.myText1, { opacity: 1, x: '0%', transform: 'translateY(0%)', duration: 1});
+        // t1.to(this.myText2, { opacity: 1, x: '0%', transform: 'translateY(0%)', duration: 2 });
+        // t1.fromTo(this.myRef3, { opacity: 1 }, { opacity: 0, duration: 0.1 }, 4)
+        //     .fromTo(this.myRef1, { opacity: 0, x: '80%' }, { opacity: 1, x: '0%', duration: 1.5 }, 4);
         
 
     }
@@ -45,13 +45,13 @@ class App extends React.Component {
                 <Footer>       
                 </Footer>
                 </div>
-                <div className={styles.start} ref={el => this.myRef2 = el}>
+                {/* <div className={styles.start} ref={el => this.myRef2 = el}>
                 </div>
 
                 <div className={styles.textanim} ref={el => this.myRef3 = el}>
                     <h1 ref={el => this.myText1 = el}>Hi</h1>
                     <h1 ref={el => this.myText2 = el}>Calm isla designed it</h1>
-                </div>
+                </div> */}
             </div>
         )
     }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import { Card, CardContent, Typography, Grid, CardActionArea, Divider } from "@material-ui/core";
+import { Button, TextField, Card, CardContent, Typography, Grid, CardActionArea, Divider } from "@material-ui/core";
 import { NavLink } from 'react-router-dom';
 
 import styles from "./HomePage.module.css";
@@ -55,9 +55,33 @@ const HomePage = () => {
             </div >
 
             <Divider style={{ backgroundColor: 'black', opacity: 0.3 }} />
-
             <div className={styles.anotherpage}>
-                <h1> another page </h1>
+                <div style={{ marginTop: '10%' }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(200, 222, 222)" fill-opacity="1" d="M0,224L48,202.7C96,181,192,139,288,117.3C384,96,480,96,576,117.3C672,139,768,181,864,192C960,203,1056,181,1152,160C1248,139,1344,117,1392,106.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+                </div>
+                <div className={styles.pagecontent}>
+                    <Typography variant="h4" style={{
+                        color: 'black', opacity: 0.85,
+                        transform: 'translateY(-200%)'
+                    }}>
+                        Members Lemon.isla can save ~15% per purchase of their shopping
+                    </Typography>
+                    <Typography variant="subtitle1" style={{
+                        color: 'black', opacity: 0.7,
+                        transform: 'translateY(-200%)'
+                    }}>
+                        You can always become a member of our community. Never overpay in future!
+                    </Typography>
+                    <div className={styles.emailsend}>
+                        <input id="enteremail" placeholder="Email" label="asdsad" className={styles.enterEmail} />
+                        <label for="enteremail" className={styles.labelEmail}>Your Email</label>
+                        <Button variant="outlined" className={styles.emailButton} >Send</Button>
+                    </div>
+                </div>
+
+                <div >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 400"><path fill="rgb(200, 222, 222)" fill-opacity="1" d="M0,224L48,202.7C96,181,192,139,288,117.3C384,96,480,96,576,117.3C672,139,768,181,864,192C960,203,1056,181,1152,160C1248,139,1344,117,1392,106.7L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+                </div>
             </div>
 
         </div>
