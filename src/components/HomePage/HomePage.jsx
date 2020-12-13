4603853ea89ea192b7a2d195e9b3fc0f31c7c460
basic-shadow <islambek.mamet@nu.edu.kz>
2020-12-13 15:43:36 -0500

    a

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -8,20 +8,25 @@ import women from '../../images/women.jpg';
 import kids from '../../images/kids.jpg';
 import { GrCaretNext, GrCaretPrevious, GrWifiNone } from 'react-icons/gr';
 import { gsap } from 'gsap';
-import img1 from '../../images/img1.jpeg';
 import makeCarousel from 'react-reveal/makeCarousel';
 import Slide from 'react-reveal/Slide';
 import styled, { css } from 'styled-components';
 import Zoom from 'react-reveal/Zoom';
 import card1 from '../../images/card1.jpeg';
-import card2 from '../../images/card2.jpeg';
+import { findAllByDisplayValue } from "@testing-library/react";
+import bags from '../../images/bags.jpg';
+import blouses from '../../images/blouses.jpg';
+import boots from '../../images/boots.jpg';
+import coats from '../../images/coats.jpeg';
+import jackets from '../../images/jackets.jpg';
+import jeans from '../../images/jeans.jpg';
+import printedshirts from '../../images/printedshirts.jpg';
 
 
 const HomePage = () => {
     const obj = { 'Men': men, 'Women': women, 'Kids': kids };
 
     const Container = styled.div`
-
     position: relative;
     overflow: hidden;
     width: 75%;
@@ -29,20 +34,16 @@ const HomePage = () => {
     `;
 
     const Arrow = styled.div`
-    line-height: 150px;
+    line-height: 100px;
     z-index: 1000;
     text-align: center;
     position: absolute;
-    top: 25%;
+    top: 30%;
     width: 10%;
     font-size: 3rem;
     cursor: pointer;
     user-select: none;
-    border-radius: 50%;
-    color: white;
-    &:hover {
-        background: linear-gradient(225deg, rgb(150, 204, 198), rgb(169, 224, 218));
-    }
+    border-radius: 30%;
 
     ${props => props.right ? css`left: 90%;` : css`left: 0%;`}
 
@@ -67,134 +68,136 @@ const HomePage = () => {
     const renderItems = () => {
         return Object.keys(obj).map((item, index) => {
             return (
-                <Card className={styles.card} key={index}>
-                    <CardActionArea>
-                        <NavLink to='/catalog'>
-                            <CardContent className={styles.cardcontent}>
-                                <img src={obj[item]} className={styles.menImg} />
-
-                                <div style={{
-                                    position: 'absolute', backgroundColor: 'rgba(255, 255, 255, 0.8)', left: '22%', top: '72%',
-                                    width: '50%', height: '10%', display: 'flex', alignItems: 'center',
-                                    justifyContent: 'center'
-                                }}>
-                                    <Typography className={styles.textMen} style={{ fontSize: '2em', fontFamily: 'Lobster, cursive' }}>{item}
-                                    </Typography>
-                                    <GrCaretNext className={styles.next} />
-
-                                </div>
-                            </CardContent>
-                        </NavLink>
-                    </CardActionArea>
-                </Card >)
+                <div className={styles.card} key={index}>
+                    <NavLink to='/catalog'>
+                        <div className={styles.cardcontent}>
+                            <img src={obj[item]} />
+                            <div className={styles.menText}>
+                                <Typography className={styles.textMen} style={{ fontSize: '2em', fontFamily: 'Lobster, cursive' }}>{item}
+                                </Typography>
+                                <GrCaretNext className={styles.next} />
+                            </div>
+                        </div>
+                    </NavLink>
+                </div>)
         });
     }
-
 
     return (
         <div>
             <div className={styles.selectionmenu}>
-                <Grid container style={{
-                    display: 'grid', gridTemplateColumns: 'repeat(3, minmax(8rem, 1fr))', justifyItems: 'center',
-                    justifyContent: 'space-between', width: '80%', height: '62vh'
-                }}>
+                <Grid container className={styles.cardBox}>
                     {renderItems()}
                 </Grid>
             </div >
 
-            <Divider style={{ backgroundColor: 'black', opacity: 0.3 }} />
-            <div style={{ display: 'grid', gridTemplateRows: '20vh' }}>
-                <label className={styles.popularcatalog}>Relevant This Season</label>
-            </div>
             <div className={styles.secondpage}>
+                <div style={{ position: 'relative', background: 'rgb(35, 34, 138)', height: '35%', width: '100%' }}>
+                </div>
+                <div className={styles.secondpageCont}>
+                    <div style={{ marginTop: '2%', marginBottom: '2%', display: 'flex', fontSize: '2.2rem' }}>
+                        <p>Relevant this</p>
+                        <p style={{ fontWeight: '700', transform: 'translateX(10%)' }}>Season</p>
+                    </div>
+                    <Carousel defaultWait='none'>
+                        <Slide right>
+                            <Zoom >
+                                <div className={styles.slides}>
+                                    <div className={styles.eachslide}>
+                                        <img src={bags} />
+                                        <p>Bags</p>
+                                    </div>
+                                    <div className={styles.eachslide}>
+                                        <img src={blouses} />
+                                        <p>Blouses</p>
+                                    </div>
+                                    <div className={styles.eachslide}>
+                                        <img src={boots} />
+                                        <p>Boots</p>
+                                    </div>
+                                </div>
+                            </Zoom>
+                        </Slide>
+                        <Slide right>
+                            <Zoom >
+                                <div className={styles.slides}>
+                                    <div className={styles.eachslide}>
+                                        <img src={coats} />
+                                        <p>Coats</p>
+                                    </div>
+                                    <div className={styles.eachslide}>
+                                        <img src={jackets} />
+                                        <p>Jackets</p>
+                                    </div>
+                                    <div className={styles.eachslide}>
+                                        <img src={jeans} />
+                                        <p>Jeans</p>
+                                    </div>
+                                </div>
+                            </Zoom>
+                        </Slide>
+                    </Carousel>
+                </div>
             </div>
-
-            <div className={styles.secondpageCont}>
-                <Carousel defaultWait='none'>
-                    <Slide right>
-                        <Zoom >
-                            <div className={styles.slides}>
-                                <div className={styles.eachslide}>
-                                    <img src={img1} />
-                                    <p>I</p>
-                                </div>
-                                <div className={styles.eachslide}>
-                                    <img src={img1} />
-                                    <p>AM</p>
-                                </div>
-                                <div className={styles.eachslide}>
-                                    <img src={img1} />
-                                    <p>GROOT</p>
-                                </div>
-                            </div>
-                        </Zoom>
-                    </Slide>
-                    <Slide right>
-                        <Zoom >
-                            <div className={styles.slides}>
-                                <div className={styles.eachslide}>
-                                    <img src={img1} />
-                                    <p>I</p>
-                                </div>
-                                <div className={styles.eachslide}>
-                                    <img src={img1} />
-                                    <p>AM</p>
-                                </div>
-                                <div className={styles.eachslide}>
-                                    <img src={img1} />
-                                    <p>isla</p>
-                                </div>
-                            </div>
-                        </Zoom>
-                    </Slide>
-                </Carousel>
-            </div>
+            <Divider style={{ height: '0.1rem', background: '#d0d9e2' }} />
 
             <div className={styles.thirdpage}>
-                <div >
-                    <div className={styles.thirdpageHeaders}>
-                        <h1>Popular</h1>
-                        <h2>Catalog</h2>
-                    </div>
-                    <div className={styles.thirdpagebox}>
-                        <div className={styles.box}>
-                            <div className={styles.imgBox}>
-                                <img src={card1} />
-                            </div>
-                            <div className={styles.contentBox}>
-                                <h2>Image Caption</h2>
-                                <p>Some text is written here</p>
+                <div className={styles.thirdpageHeaders}>
+                    <h1>Popular</h1>
+                    <h2>Catalog</h2>
+                </div>
+                <div className={styles.thirdpagebox}>
+                    <div className={styles.box} >
+                        <div className={styles.imgBox1}>
+                            <img src={printedshirts} />
+                        </div>
+                        <div className={styles.contentBox1}>
+                            <div className={styles.content1}>
+                                <h2>Printed men's shirts</h2>
+                                <p>Opportunity to create your own print in chosen shir qweqwet</p>
                             </div>
                         </div>
-
-                        <div className={styles.box}>
-                            <div className={styles.imgBox}>
-                                <img src={card1} />
+                    </div>
+                    <div className={styles.box} >
+                        <div className={styles.imgBox2}>
+                            <img src={printedshirts} />
+                        </div>
+                        <div className={styles.contentBox2}>
+                            <div className={styles.content2}>
+                                <h2>Printed men's shirts</h2>
+                                <p>Opportunity to create your own print in chosen shir qweqwet</p>
                             </div>
-                            <div className={styles.contentBox}>
-                                <h2>Image Caption</h2>
-                                <p>Some text is written here</p>
+                        </div>
+                    </div>
+                    <div className={styles.box} >
+                        <div className={styles.imgBox1}>
+                            <img src={printedshirts} />
+                        </div>
+                        <div className={styles.contentBox1}>
+                            <div className={styles.content1}>
+                                <h2>Printed men's shirts</h2>
+                                <p>Opportunity to create your own print in chosen shir qweqwet</p>
                             </div>
                         </div>
                     </div>
                 </div>
-
+                <div className={styles.area} >
+                </div >
             </div>
-
 
             <div className={styles.prelastpage}>
                 <div style={{ marginTop: '10%' }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(200, 222, 222)" fill-opacity="1" d="M0,224L48,202.7C96,181,192,139,288,117.3C384,96,480,96,576,117.3C672,139,768,181,864,192C960,203,1056,181,1152,160C1248,139,1344,117,1392,106.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#455066" fill-opacity="1" d="M0,224L48,202.7C96,181,192,139,288,117.3C384,96,480,96,576,117.3C672,139,768,181,864,192C960,203,1056,181,1152,160C1248,139,1344,117,1392,106.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
                 </div>
                 <div className={styles.pagecontent}>
                     <Typography variant="h4" style={{
-                        color: 'black', opacity: 0.85,
+                        color: '#fff', opacity: 0.85,
                         transform: 'translateY(-200%)'
                     }}>
                         Members Lemon.isla can save ~15% per purchase of their shopping
                     </Typography>
                     <Typography variant="subtitle1" style={{
-                        color: 'black', opacity: 0.7,
+                        color: '#fff', opacity: 0.7,
                         transform: 'translateY(-200%)'
                     }}>
                         You can always become a member of our community. Never overpay in future!
@@ -206,7 +209,7 @@ const HomePage = () => {
                     </div>
                 </div>
                 <div >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 400"><path fill="rgb(200, 222, 222)" fill-opacity="1" d="M0,224L48,202.7C96,181,192,139,288,117.3C384,96,480,96,576,117.3C672,139,768,181,864,192C960,203,1056,181,1152,160C1248,139,1344,117,1392,106.7L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 400"><path fill="#455066" fill-opacity="1" d="M0,224L48,202.7C96,181,192,139,288,117.3C384,96,480,96,576,117.3C672,139,768,181,864,192C960,203,1056,181,1152,160C1248,139,1344,117,1392,106.7L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
                 </div>
             </div>
 
